455dff4a9f870e5cdb3be3c181873e5ad9a6cce2
S <regspamreg@yandex.ru>
2023-11-27 15:59:09 +0300

    Начинаем работу по переходу на новую версию протокола. Обусловлено появлением новой версии документа Д58.

--- src/Uart.h
+++ src/Uart.h
@@ -52,8 +52,8 @@
 // Размеры пакетов, отправляемые ЗРУ по RS485 
 #define lngPackRs1					19														// телеметрия ЗРУ
 #define lngPackRs2					6															// Ответ на пакет 2
-#define lngPackRs3					15														// краткая телеметрия БЭ
-#define lngPackRs4					88														// полная телеметрия БЭ
+#define lngPackRs3					29														// краткая телеметрия БЭ
+#define lngPackRs4					90														// полная телеметрия БЭ
 #define lngPackRs5					6															// Ответ на пакет 5
 #define lngPackRs6					36														// контроль уставок ЗРУ 
 #define lngPackRs7					10														// запоминаемые для восстановления данные в БВС 
